f1d4c3d2e882829e203dbaf1ea4a318ae5a05c40
gonewbie <gounewbie@gmail.com>
2019-12-18 22:06:59 +0900

    modify: Apollo Setup Recap

--- src/apollo.ts
+++ src/apollo.ts
@@ -7,6 +7,34 @@ const apolloClient = new ApolloClient({
         __typename: 'Auth',
         isLoggedIn: Boolean(localStorage.getItem('jwt'))
       }
+    },
+    resolvers: {
+      Mutation: {
+        logUserIn: (_, { token }, { cache }) => {
+          localStorage.setItem('jwt', token);
+          cache.writeData({
+            data: {
+              auth: {
+                __typename: 'Auth',
+                isLoggedIn: true,
+              }
+            }
+          });
+          return null;
+        },
+        logUserOut: (_, __, { cache }) => {
+          localStorage.removeItem('jwt');
+          cache.writeData({
+            data: {
+              auth: {
+                __typename: 'Auth',
+                isLoggedIn: false,
+              }
+            }
+          });
+          return null;
+        },
+      }
     }
   },
   request: async (operation: Operation) => {
