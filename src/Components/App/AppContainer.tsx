34cb3466b99ebd194645f7f5b7f01c64ea69103e
gonewbie <gounewbie@gmail.com>
2019-12-19 21:27:28 +0900

    add: Typescript and Styled Components set up

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
+import reset from 'styled-reset';
+import { theme } from '../../theme';
+import { ThemeProvider, createGlobalStyle } from '../../typed-components';
 import AppPresenter from './AppPresenter';
 import { IS_LOGGED_IN } from './AppQueries';
 
+const GlobalStyle = createGlobalStyle`${reset}`;
+
 const AppContainer:any = ({ data }) => (
-  <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <AppPresenter isLoggedIn={data.auth.isLoggedIn} />
+    </ThemeProvider>
+  </>
 );
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
