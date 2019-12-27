5021117a40430436a416954f14e22af7e2c4198d
gonewbie <gounewbie@gmail.com>
2019-12-28 01:31:43 +0900

    Ride Status Subscription

--- src/routes/Ride/RideContainer.tsx
+++ src/routes/Ride/RideContainer.tsx
@@ -1,9 +1,10 @@
+import { SubscribeToMoreOptions } from "apollo-client";
 import React from 'react';
 import { Mutation, Query } from 'react-apollo';
 import { RouteComponentProps } from 'react-router-dom';
 import {USER_PROFILE} from "../../sharedQueries.queries";
 import {getRide, getRideVariables, updateRide, updateRideVariables, userProfile} from "../../types/api";
-import {GET_RIDE, UPDATE_RIDE_STATUS} from "./Ride.queries";
+import {GET_RIDE, RIDE_SUBSCRIPTION, UPDATE_RIDE_STATUS} from "./Ride.queries";
 import RidePresenter from "./RidePresenter";
 
 interface IProps extends RouteComponentProps<any> {}
@@ -31,24 +32,53 @@ class RideContainer extends React.Component<IProps> {
       <Query<userProfile> query={USER_PROFILE}>
         {({ data: userData }) => (
           <Query<getRide, getRideVariables> query={GET_RIDE} variables={{ rideId: parseInt(rideId, 10) }}>
-            {({ data: rideData }) => (
-              <Mutation<updateRide, updateRideVariables>
-                mutation={UPDATE_RIDE_STATUS}
-                refetchQueries={[{ query: GET_RIDE, variables: { rideId: parseInt(rideId, 10) } }]}
-              >
-                {updateRideMutation => (
-                  <RidePresenter
-                    rideData={rideData}
-                    userData={userData}
-                    updateRideMutation={updateRideMutation}
-                  />
-                )}
-              </Mutation>
-            )}
+            {({ data: rideData, loading, subscribeToMore }) => {
+              const subscribeOptions: SubscribeToMoreOptions = {
+                document: RIDE_SUBSCRIPTION,
+                updateQuery: (prev, { subscriptionData }) => {
+                  if (!subscriptionData.data) {
+                    return prev;
+                  }
+                  const {
+                    data: {
+                      RideStatusSubscription: { status }
+                    }
+                  } = subscriptionData;
+                  if (status === 'FINISHED') {
+                    window.location.href='/';
+                  }
+                }
+              }
+              subscribeToMore(subscribeOptions);
+              return (
+                <Mutation<updateRide, updateRideVariables>
+                  mutation={UPDATE_RIDE_STATUS}
+                  refetchQueries={[{ query: GET_RIDE, variables: { rideId: parseInt(rideId, 10) } }]}
+                  onCompleted={() => this.handleRideUpdate(rideData)}
+                >
+                  {updateRideMutation => (
+                    <RidePresenter
+                      rideData={rideData}
+                      userData={userData}
+                      updateRideMutation={updateRideMutation}
+                    />
+                  )}
+                </Mutation>
+              )
+            }}
           </Query>
         )}
       </Query>
     );
+  }
+
+  public handleRideUpdate(rideData) {;
+    const {
+      GetRide
+    } = rideData;
+    if (GetRide && GetRide.ride.status === 'ONROUTE') {
+      window.location.href='/';
+    }
   }
 }
 
